7dfb7219fb8b1d1514e8827520e269ea82d0fdf2
atharmohammad <mohd.rule123@gmail.com>
2021-01-30 20:12:53 +0530

    Persistent Authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React,{Component} from 'react'
 import Layout from './components/Layout/Layout.js'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder.js'
-import {BrowserRouter,Route} from 'react-router-dom'
+import {BrowserRouter,Route,withRouter} from 'react-router-dom'
 import Checkout from './components/Order/Checkout/Checkout'
 import Orders from './components/Order/Orders/Orders'
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
+import * as actionCreator from  './store/index'
+import {connect} from 'react-redux';
+
 class App extends Component {
+
+  componentDidMount = ()=>{
+    this.props.persistentAuthenticating();
+  }
+
   render(){
     return(
       <BrowserRouter>
@@ -24,4 +32,10 @@ class App extends Component {
   }
 };
 
-export default App;
+const mapDispatchToProps = dispatch =>{
+  return{
+    persistentAuthenticating : ()=>dispatch(actionCreator.persistentAuthenticating())
+  }
+}
+
+export default (connect(null,mapDispatchToProps)(App));
